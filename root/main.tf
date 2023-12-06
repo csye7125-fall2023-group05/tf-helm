c9beaa2ad08311338c25d54f9a4176d998e15649
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-12-06 03:37:53 -0500

    feat: install istio service mesh
    - add dynamic namespace creation/deletion with terraform k8s provider
    - add `istio-injection` labels to namespaces on creation
    - introduced modules to install Istio helm charts
      - `istio/base`
      - `istio/istiod`
      - `istio/gateway`

--- root/main.tf
+++ root/main.tf
@@ -1,4 +1,40 @@
+module "webapp_namespace" {
+  source = "../modules/namespace"
+}
+
+module "istio_base" {
+  depends_on = [module.webapp_namespace]
+  source     = "../modules/istio_base"
+}
+
+resource "time_sleep" "istall_istio_crds" {
+  depends_on      = [module.istio_base]
+  create_duration = "20s"
+}
+
+module "istio_discovery" {
+  depends_on = [time_sleep.istall_istio_crds]
+  source     = "../modules/istiod"
+}
+
+resource "time_sleep" "istall_istio_discovery" {
+  depends_on      = [module.istio_discovery]
+  create_duration = "20s"
+}
+
+module "istio_gateway" {
+  depends_on = [time_sleep.istall_istio_discovery]
+  source     = "../modules/istio_gateway"
+}
+
+
+resource "time_sleep" "istall_istio_gateway" {
+  depends_on      = [module.istio_gateway]
+  create_duration = "20s"
+}
+
 module "infra_dependencies" {
+  depends_on        = [time_sleep.istall_istio_gateway]
   source            = "../modules/infra_helm"
   timeout           = var.timeout
   infra_values_file = var.infra_values_file
